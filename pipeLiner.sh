8c2407b79f784778aeefdda169a690c9d038a389
Nick Guyer <guyern@gmail.com>
2017-06-16 01:23:04 -0500

    Updated Jenkinsfile and pipeLiner.sh

--- pipeLiner.sh
+++ pipeLiner.sh
@@ -2,7 +2,6 @@
 # name: pipeLiner.sh
 # author: Nick Guyer
 # email: guyern@gmail.com
-# version: 0.1.0
 
 set -o errexit
 set -o nounset
@@ -26,9 +25,39 @@ function RunTest {
     echo "################################"
 }
 
+function Usage {
+    echo "NAME:"
+    echo -e "\t pipeLiner.sh - Script to setup jenkins pipelines"
+    echo ""
+    echo "USAGE:"
+    echo -e "\t [environment options] ./pipeLiner.sh command"
+    echo ""
+    echo "VERSION:"
+    echo -e "\t 0.1.0"
+    echo ""
+    echo "COMMANDS:"
+    echo -e "\t -r      Gather Report Information - Runs a report"
+    echo -e "\t -s      Syntax - Runs a syntax check on an ansible playbooks"
+}
+
 function Syntax {
     find ./roles -name '*.yml' -maxdepth 3 | xargs -n1 yamllint -d relaxed -f parsable
 }
 
-RunTest
-Syntax
+# if no arguments are passed
+if [ $# == 0 ];then
+    Usage
+    exit 1
+fi
+
+case "$1" in
+# sets up ansible library
+-r)     RunTest
+        ;;
+# sets up environment
+-s)     Syntax
+        ;;
+# anything else go to Usage function
+*)      Usage
+        ;;
+esac
